239fb4d1b185dfc92ba3ac97917256b7e574262a
Chiriius <gonzalezmigueln@gmail.com>
2024-05-15 09:08:36 -0500

    Cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app";
+const cors = require('cors');
 
 
 const PORT = process.env.PORT;
+app.use(cors({"Access-Control-Allow-Origin":"*"}));
 app.listen(PORT,()=>{
     console.log(`Server is running on Port: ${PORT}`)
 
